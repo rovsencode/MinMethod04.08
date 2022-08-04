aad58b8ddcb6393001a7a1ea173290d5f57aeea2
rovsencode <rovshanakh@code.edu.az>
2022-08-04 16:44:54 +0400

    homework

---
+++ MinMethodTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MinMethodTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 14, 16, 17, 19, 25, 2, 124, 154, 129 };
+            Console.WriteLine(Min(array));
+        }
+        static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i=1;i<array.Length;i++)
+            {
+
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+                
+            }
+            return min ;
+        }
+    }
+}
